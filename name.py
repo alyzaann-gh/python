d3ca65815830fa427682792f7e5db0895d5fafeb
alyzaann-gh <bulyzaann5@gmail.com>
2025-08-12 13:05:51 +0800

    Update name.py

---
+++ name.py
@@ -0,0 +1,7 @@
+name1 = input("Name of the first classmate: ")
+name2 = input("Name of the second classmate: ")
+name3 = input("Name of the third classmate: ")
+
+print(name1)
+print(name2)
+print(name3)
